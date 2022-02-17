42f29bc1f5d21d3ffe07087f58c56ffc3ba3a27d
dwcalade <95244420+dwcalade@users.noreply.github.com>
2022-02-17 09:52:08 -0800

    Whichschool Function
    Whichschool Function

---
+++ whichschool-lighthouse.js
@@ -0,0 +1,18 @@
+function whichSchool(age) {
+  // Your code in here ...
+  if (age < 13) { 
+
+    return ('Elementary School');
+  }
+
+   else if (age >= 13 && age <= 18) { 
+
+    return ('Secondary School');
+  }
+
+  else if (age >= 19) {
+
+    return ('Lighthouse Labs');
+
+  }
+}
